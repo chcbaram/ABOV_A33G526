5791f940b8d4c9ae5761d136a0935ad055f06263
Baram <chcbaram@gmail.com>
2021-07-25 20:21:21 +0900

    디버깅 프로젝트 추가.

---
+++ a33g526_fw/src/main.c
@@ -0,0 +1,24 @@
+/*
+ * main.c
+ *
+ *  Created on: 2021. 7. 25.
+ *      Author: baram
+ */
+
+
+#include "main.h"
+
+
+
+
+
+int main(void)
+{
+
+  while(1)
+  {
+
+  }
+
+  return 0;
+}
---
+++ a33g526_fw/src/main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ *
+ *  Created on: 2021. 7. 25.
+ *      Author: baram
+ */
+
+#ifndef SRC_MAIN_H_
+#define SRC_MAIN_H_
+
+
+
+#endif /* SRC_MAIN_H_ */
