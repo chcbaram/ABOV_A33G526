61697d203d900198ee72c387f50fe88c227a3111
Baram <chcbaram@gmail.com>
2021-08-01 00:27:08 +0900

    UART 로그  출력 추가.

--- a33g526_fw/src/hw/hw.c
+++ a33g526_fw/src/hw/hw.c
@@ -21,5 +21,9 @@ bool hwInit(void)
   uartInit();
   uartOpen(_DEF_UART1, 57600);
 
+  logPrintf("[ Firmware Begin... ]\r\n");
+  logPrintf("Booting..Name \t\t: %s\r\n", _DEF_BOARD_NAME);
+  logPrintf("Booting..Ver  \t\t: %s\r\n", _DEF_FIRMWATRE_VERSION);
+
   return true;
 }
